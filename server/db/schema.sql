1254df7c63cc992e3623915131d9f5690d119144
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-12 10:42:33 -0700

    complete initial file structure and server doc

---
+++ server/db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS fec;
+CREATE DATABASE fec;
+
+\c fec;
+
+CREATE TABLE users (
+  ID SERIAL PRIMARY KEY,
+  firstname VARCHAR,
+  lastname VARCHAR,
+  imageUrl VARCHAR
+);
+
+CREATE TABLE listings (
+  ID SERIAL PRIMARY KEY,
+  overall, DECIMAL,
+  accuracy DECIMAL,
+  communication DECIMAL,
+  cleanliness DECIMAL,
+  location DECIMAL,
+  value DECIMAL
+);
+
+CREATE TABLE reviews (
+  ID SERIAL PRIMARY KEY,
+  user_id VARCHAR,
+  listing_id INT,
+  body TEXT,
+  accuracy INT,
+  location INT,
+  cleanliness INT,
+  communication INT,
+  value INT,
+  isFlagged INT
+);
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// logs requests to the console
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+app.listen(3000, () => {
+  console.log('server running at: http://localhost:3000');
+});
+
+module.exports = app;
