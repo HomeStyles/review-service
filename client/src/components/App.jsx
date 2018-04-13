aea2493524289d51b1598d2b082c849ffb24b065
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-13 10:32:25 -0700

    Complete webpack setup with boilerplate rendering

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
