8473c2eb2568d6ca04633460ed9954ae716c6a9d
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-16 10:28:47 -0700

    completes basic setup of overview comonent

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 const Overview = props => (
   <div id="overview">
-    Overview goes here
+    <div className="category">
+      <span>Accuracy</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Communication</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Cleanliness</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Location</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Check In</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Value</span>
+      <StarRatings
+        rating={4.5}
+        starRatedColor='rgb(0,132,137)'
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
   </div>
 );
 
