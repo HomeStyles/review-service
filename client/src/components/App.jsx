93d77091aa2359164802c96475aa82b9422b75be
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-16 15:04:42 -0700

    remove dummy data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,11 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      review: {
-        imageurl: "https://s3.amazonaws.com/uifaces/faces/twitter/_victa/128.jpg",
-        name: "Andrew",
-        body: "Suscipit nihil quisquam blanditiis. Laudantium inventore placeat ullam error illum maxime temporibus totam. Placeat rerum cumque. Error omnis velit laboriosam. Ex ex suscipit vel et fugit. Atque officia fugiat minus consequuntur blanditiis. Aut necessitatibus et perspiciatis dolore et perspiciatis quia quia dolores. Consequatur consequatur labore. Cum molestiae nam. Non qui eos ipsum laudantium tempore numquam debitis nam. Debitis ut ex quia autem cupiditate perferendis esse numquam accusantium.",
-      },
+
     };
   }
 
