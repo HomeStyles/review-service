cb27c2f5f6199bf5fb64488af43516a27674899e
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-16 11:36:28 -0700

    complete basic overview with passed-in dummy data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,23 @@ import Review from './Review';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      overview: {
+        accuracy: 4.2,
+        communication: 3.8,
+        cleanliness: 4.5,
+        location: 3.9,
+        checkin: 4.8,
+        value: 3.7
+      }
+    };
   }
 
   render() {
     return (
       <div className="reviewsHolder">
         <Header />
-        <Overview />
+        <Overview overview={this.state.overview}/>
         <Review />
 
       </div>
--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 const Overview = props => (
   <div id="overview">
-    Overview goes here
+    <div className="category">
+      <span>Accuracy</span>
+      <StarRatings
+        rating={props.overview.accuracy}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Communication</span>
+      <StarRatings
+        rating={props.overview.communication}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Cleanliness</span>
+      <StarRatings
+        rating={props.overview.cleanliness}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Location</span>
+      <StarRatings
+        rating={props.overview.location}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Check In</span>
+      <StarRatings
+        rating={props.overview.checkin}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
+    <div className="category">
+      <span>Value</span>
+      <StarRatings
+        rating={props.overview.value}
+        starRatedColor="rgb(0,132,137)"
+        starDimension="20px"
+        starSpacing="2px"
+      />
+    </div>
   </div>
 );
 
