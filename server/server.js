270b30a265679e028ebb42eda7c2ada5362584cd
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-25 16:23:56 -0700

    complete redis caching

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const bluebird = require('bluebird');
+const redis = require('redis');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+const redisClient = redis.createClient(6379, 'localhost');
 const app = express();
 
 // logs requests to the console
@@ -16,17 +20,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/listings/:tagId', (req, res) => {
-  const listingData = {};
-  db.any('SELECT DISTINCT reviews.body, reviews.overall, users.firstname, users.imageurl FROM reviews JOIN users ON reviews.user_id = users.id WHERE reviews.listing_id = $1', [req.params.tagId])
-    .then((data) => {
-      listingData.reviews = data;
-      return db.any('SELECT * from listings where id = $1', [req.params.tagId]);
-    })
-    .then((data) => {
-      listingData.overview = data;
-    })
-    .then(() => {
-      res.status(200).send(listingData);
+  redisClient.getAsync(req.params.tagId)
+    .then((cachedListing) => {
+      if (cachedListing !== null) {
+        console.log('found a cached listing', cachedListing)
+        res.status(200).send(cachedListing);
+      } else {
+        const listingData = {};
+        db.any('SELECT DISTINCT reviews.body, reviews.overall, users.firstname, users.imageurl FROM reviews JOIN users ON reviews.user_id = users.id WHERE reviews.listing_id = $1', [req.params.tagId])
+          .then((data) => {
+            listingData.reviews = data;
+            return db.any('SELECT * from listings where id = $1', [req.params.tagId]);
+          })
+          .then((data) => {
+            listingData.overview = data;
+          })
+          .then(() => {
+            res.status(200).send(listingData);
+            const listingString = JSON.stringify(listingData);
+            redisClient.set(req.params.tagId, listingString)
+          })
+          .catch((error) => {
+            res.status(200).send({
+              message: error,
+            });
+          });
+      }
     })
     .catch((error) => {
       res.status(200).send({
